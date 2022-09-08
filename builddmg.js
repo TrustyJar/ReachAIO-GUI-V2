6b2b53a07b42c8c24da278478a25e1126557d78b
TrustyJar <88812001+TrustyJar@users.noreply.github.com>
2022-09-08 00:24:07 -0400

    fixed app name

--- builddmg.js
+++ builddmg.js
@@ -3,7 +3,7 @@ const createDMG = require('electron-installer-dmg');
 async function buildDMG() {
     await createDMG({
       appPath: './reachaio-gui-v2-master-darwin-arm64/ReachAIO.app',
-      name: 'MyApp'
+      name: 'ReachAIO'
     });
 }
 
